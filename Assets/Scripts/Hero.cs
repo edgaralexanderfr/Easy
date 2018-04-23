8b58673d69d86c77a6ee24bbfe1529efab5a9f54
Edgar Alexander Franco <edgaralexanderfr@gmail.com>
2018-04-23 00:02:58 -0500

    Initial commit.

---
+++ Assets/Scripts/Hero.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hero : MonoBehaviour {
+
+	private GameObject TorsoArmature;
+
+	// Use this for initialization
+	void Start () {
+		this.TorsoArmature = GameObject.Find("TorsoArmature");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		int speed = 1000;
+
+		if (Input.GetKeyUp("up")) {
+			this.TorsoArmature.transform.Rotate(Vector3.left * Time.deltaTime * speed);
+		}
+
+		if (Input.GetKeyUp("down")) {
+			this.TorsoArmature.transform.Rotate(Vector3.right * Time.deltaTime * speed);
+		}
+
+		if (Input.GetKeyUp("left")) {
+			this.TorsoArmature.transform.Rotate(Vector3.down * Time.deltaTime * speed, Space.World);
+		}
+
+		if (Input.GetKeyUp("right")) {
+			this.TorsoArmature.transform.Rotate(Vector3.up * Time.deltaTime * speed, Space.World);
+		}
+	}
+}
