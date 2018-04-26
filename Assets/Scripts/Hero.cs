8192b564ed70cd06dde5fe68b63f12be9000cfed
Edgar Alexander Franco <edgaralexanderfr@gmail.com>
2018-04-25 22:25:01 -0500

    1. Add third person mode. 2. Play walk animation only when moving.

--- Assets/Scripts/Hero.cs
+++ Assets/Scripts/Hero.cs
@@ -7,12 +7,23 @@ public class Hero : MonoBehaviour {
 	const float AIM_SPEED = 10.0f;
 	const float MOVE_SPEED = 20.0f;
 
+	[SerializeField]
+	private Camera MainCamera;
+	[SerializeField]
+	private Camera ThirdPersonCamera;
+	
+	private Animator HeroAnimator;
 	private GameObject TorsoArmature;
 	private Vector3 MoveDirection;
+	private bool PlayingWalkAnimation = false;
 
 	// Use this for initialization
 	void Start () {
+		this.HeroAnimator = this.GetComponent<Animator>();
 		this.TorsoArmature = GameObject.Find("TorsoArmature");
+
+		this.MainCamera.enabled = true;
+		this.ThirdPersonCamera.enabled = false;
 	}
 	
 	// Update is called once per frame
@@ -23,21 +34,44 @@ public class Hero : MonoBehaviour {
 		this.MoveDirection.x = 0.0f;
 		this.MoveDirection.y = this.TorsoArmature.transform.rotation.eulerAngles.y;
 		this.MoveDirection.z = 0.0f;
+
+		bool moving = false;
 		
 		if (Input.GetKey(KeyCode.W)) {
 			this.transform.Translate(Quaternion.Euler(this.MoveDirection) * Vector3.forward * Time.deltaTime * MOVE_SPEED);
+			moving = true;
 		}
 
 		if (Input.GetKey(KeyCode.S)) {
 			this.transform.Translate(Quaternion.Euler(this.MoveDirection) * Vector3.back * Time.deltaTime * MOVE_SPEED);
+			moving = true;
 		}
 
 		if (Input.GetKey(KeyCode.A)) {
 			this.transform.Translate(Quaternion.Euler(this.MoveDirection) * Vector3.left * Time.deltaTime * MOVE_SPEED);
+			moving = true;
 		}
 
 		if (Input.GetKey(KeyCode.D)) {
 			this.transform.Translate(Quaternion.Euler(this.MoveDirection) * Vector3.right * Time.deltaTime * MOVE_SPEED);
+			moving = true;
+		}
+
+		if (Input.GetKeyUp(KeyCode.F7)) {
+			this.MainCamera.enabled = !this.MainCamera.enabled;
+			this.ThirdPersonCamera.enabled = !this.ThirdPersonCamera.enabled;
+		}
+
+		if (moving) {
+			if (!this.PlayingWalkAnimation) {
+				this.HeroAnimator.Play("Walk");
+				this.PlayingWalkAnimation = true;
+			}
+		} else {
+			if (this.PlayingWalkAnimation) {
+				this.HeroAnimator.Play("Idle");
+				this.PlayingWalkAnimation = false;
+			}
 		}
 	}
 }
